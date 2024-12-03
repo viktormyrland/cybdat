eb8f3e4e65be13a9290ffa3406beebd9e098878e
Viktor Myrland <viktormyrland@gmail.com>
2024-12-03 23:43:17 +0100

    more stuff

--- src/app/_components/block.tsx
+++ src/app/_components/block.tsx
@@ -1,19 +1,32 @@
 import Link from "next/link";
 import React from "react";
+import SchoolIcon from "@mui/icons-material/School";
+import TerminalIcon from "@mui/icons-material/Terminal";
 
 export interface BlockParams {
   title: string;
   url: string;
+  icon?: string;
 }
 
 export default function Block(params: { block: BlockParams }) {
+  let icon = undefined;
+  switch (params.block.icon) {
+    case "school":
+      icon = <SchoolIcon style={{ width: "100%", height: "100%" }} />;
+      break;
+    case "terminal":
+      icon = <TerminalIcon style={{ width: "100%", height: "100%" }} />;
+  }
+
   return (
     <Link
       href={params.block.url}
       className="transition-transform hover:scale-105"
     >
-      <div className="h-36 w-36 border border-[#FFDAAD] bg-[#FFF6EB] p-2">
+      <div className="flex h-36 w-36 flex-col items-center gap-3 border border-[#FFDAAD] bg-[#FFF6EB] p-2">
         <h2 className="text-center">{params.block.title}</h2>
+        {icon !== undefined && <div className="w-20 text-[#444]">{icon}</div>}
       </div>
     </Link>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,24 @@
-import { HydrateClient } from "~/trpc/server";
+"use client";
 import Block from "./_components/block";
+import { Oxanium, Lato } from "next/font/google";
+
+const oxanium = Oxanium({ weight: "600" });
+const lato = Lato({ weight: "400" });
 
 const blocks = [
   {
     title: "Studentweb",
     url: "https://fsweb.no/studentweb/login.jsf?inst=FSNTNU",
+    icon: "school",
   },
   {
     title: "Matematikk 3",
     url: "https://wiki.math.ntnu.no/tma4110/2024h/start",
   },
   {
-    title: "Objektorientert Programmering",
+    title: "OOP",
     url: "https://ntnu.blackboard.com/ultra/courses/_46789_1/cl/outline",
+    icon: "terminal",
   },
   {
     title: "Abakus",
@@ -20,23 +26,20 @@ const blocks = [
   },
 ];
 
-export default async function Home() {
+export default function Home() {
   return (
-    <HydrateClient>
-      {/* <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]"></h1>
-        </div>
-      </main> */}
-      <main className="flex h-screen w-screen flex-col items-center bg-gradient-to-b from-[#533A71] to-[#26202E]">
-        {/* <div className="h-[50px] w-full bg-red-500"></div> */}
-        <div className="flex h-full w-[80%] max-w-[1200px] flex-row flex-wrap content-start justify-center gap-3 border border-white p-6">
-          {[...blocks, ...blocks, ...blocks, ...blocks].map((b, _index) => {
+    <main className="flex h-full min-h-screen w-screen flex-col items-center justify-between bg-gradient-to-b from-[#533A71] to-[#26202E]">
+      <div className="flex w-full flex-col items-center gap-3 pt-6">
+        <h1 className={`${oxanium.className} text-5xl text-white`}>CYBDAT</h1>
+        <div
+          className={`flex h-full w-[95%] max-w-[1500px] flex-row flex-wrap content-start justify-center gap-3 p-6 ${lato.className}`}
+        >
+          {blocks.map((b, _index) => {
             return <Block key={_index} block={b} />;
           })}
         </div>
-        <div className="h-[50px] w-full bg-red-500"></div>
-      </main>
-    </HydrateClient>
+      </div>
+      <div className="h-[50px] w-full bg-red-500"></div>
+    </main>
   );
 }
