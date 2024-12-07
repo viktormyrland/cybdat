e1ab68dc119baf2123e9e0f2a86c8aab78f4be3d
Viktor Myrland <viktormyrland@gmail.com>
2024-12-07 18:01:24 +0100

    touches

--- src/app/_components/block.tsx
+++ src/app/_components/block.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import React from "react";
 import SchoolIcon from "@mui/icons-material/School";
 import TerminalIcon from "@mui/icons-material/Terminal";
+import CircleIcon from "@mui/icons-material/Circle";
+import CalculateIcon from "@mui/icons-material/Calculate";
 
 export interface BlockParams {
   title: string;
@@ -17,6 +19,12 @@ export default function Block(params: { block: BlockParams }) {
       break;
     case "terminal":
       icon = <TerminalIcon style={{ width: "100%", height: "100%" }} />;
+      break;
+    case "circle":
+      icon = <CircleIcon style={{ width: "95%", height: "95%" }} />;
+      break;
+    case "math":
+      icon = <CalculateIcon style={{ width: "95%", height: "95%" }} />;
   }
 
   return (
@@ -26,7 +34,11 @@ export default function Block(params: { block: BlockParams }) {
     >
       <div className="flex h-36 w-36 flex-col items-center gap-3 border border-[#FFDAAD] bg-[#FFF6EB] p-2">
         <h2 className="text-center">{params.block.title}</h2>
-        {icon !== undefined && <div className="w-20 text-[#444]">{icon}</div>}
+        {icon !== undefined && (
+          <div className="flex w-20 items-center justify-center text-[#444]">
+            {icon}
+          </div>
+        )}
       </div>
     </Link>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,35 @@ const lato = Lato({ weight: "400", subsets: ["latin"] });
 
 const blocks = [
   {
+    title: "Matematikk 3",
+    url: "https://wiki.math.ntnu.no/tma4115/2025v/start",
+    icon: "math",
+  },
+  {
+    title: "Objektorientert",
+    url: "https://www.ntnu.no/wiki/display/tdt4100",
+    icon: "terminal",
+  },
+  {
     title: "Studentweb",
     url: "https://fsweb.no/studentweb/login.jsf?inst=FSNTNU",
     icon: "school",
   },
   {
-    title: "Matematikk 3",
-    url: "https://wiki.math.ntnu.no/tma4110/2024h/start",
-  },
-  {
-    title: "OOP",
-    url: "https://ntnu.blackboard.com/ultra/courses/_46789_1/cl/outline",
-    icon: "terminal",
-  },
-  {
     title: "Abakus",
     url: "https://abakus.no",
+    icon: "circle",
   },
 ];
 
 export default function Home() {
   return (
     <main className="flex h-full min-h-screen w-screen flex-col items-center justify-between bg-gradient-to-b from-[#533A71] to-[#26202E]">
-      <div className="flex w-full flex-col items-center gap-3 pt-6">
+      <div className="flex w-full flex-col items-center gap-2 pt-6">
         <h1 className={`${oxanium.className} text-5xl text-white`}>CYBDAT</h1>
+        <h3 className={`text-md font-[Consolas] font-bold text-[#aaa]`}>
+          Cybersikkerhet og Datakommunikasjon ved NTNU
+        </h3>
         <div
           className={`flex h-full w-[95%] max-w-[1500px] flex-row flex-wrap content-start justify-center gap-3 p-6 ${lato.className}`}
         >
@@ -39,7 +44,7 @@ export default function Home() {
           })}
         </div>
       </div>
-      <div className="h-[50px] w-full bg-red-500"></div>
+      {/* <div className="h-[50px] w-full bg-red-500"><h3></h3></div> */}
     </main>
   );
 }
