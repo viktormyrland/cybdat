cfd23097e14afc0b5d5e65d80ecf28970ecf5132
Viktor Myrland <viktormyrland@gmail.com>
2024-12-10 14:12:28 +0100

    Revert "Moved definition of icon color"
    This reverts commit 73bc385850ba3bc3b1faceeb013fe2c3929aa652.

--- src/app/_components/block.tsx
+++ src/app/_components/block.tsx
@@ -14,32 +14,66 @@ export interface BlockParams {
   icon?: string;
   subtitle?: string;
   titleClasses?: string;
-  iconColor?: string;
 }
 
 export default function Block(params: { block: BlockParams }) {
   let icon = undefined;
   switch (params.block.icon) {
     case "school":
-      icon = <SchoolIcon style={{ width: "100%", height: "100%" }} />;
+      icon = (
+        <SchoolIcon
+          className="text-[#874600]"
+          style={{ width: "100%", height: "100%" }}
+        />
+      );
       break;
     case "terminal":
-      icon = <TerminalIcon style={{ width: "100%", height: "100%" }} />;
+      icon = (
+        <TerminalIcon
+          className="text-[#33333]"
+          style={{ width: "100%", height: "100%" }}
+        />
+      );
       break;
     case "circle":
-      icon = <CircleIcon style={{ width: "95%", height: "95%" }} />;
+      icon = (
+        <CircleIcon
+          className="text-[#cc0000]"
+          style={{ width: "95%", height: "95%" }}
+        />
+      );
       break;
     case "math":
-      icon = <CalculateIcon style={{ width: "95%", height: "95%" }} />;
+      icon = (
+        <CalculateIcon
+          className="text-[#2d1896]"
+          style={{ width: "95%", height: "95%" }}
+        />
+      );
       break;
     case "science":
-      icon = <ScienceIcon style={{ width: "95%", height: "95%" }} />;
+      icon = (
+        <ScienceIcon
+          className="text-[#44ab4d]"
+          style={{ width: "95%", height: "95%" }}
+        />
+      );
       break;
     case "lab":
-      icon = <BiotechIcon style={{ width: "95%", height: "95%" }} />;
+      icon = (
+        <BiotechIcon
+          className="text-[#44ab4d]"
+          style={{ width: "95%", height: "95%" }}
+        />
+      );
       break;
     case "assignment":
-      icon = <AssignmentIcon style={{ width: "95%", height: "95%" }} />;
+      icon = (
+        <AssignmentIcon
+          className="text-[#118099]"
+          style={{ width: "95%", height: "95%" }}
+        />
+      );
       break;
   }
 
@@ -48,13 +82,13 @@ export default function Block(params: { block: BlockParams }) {
       href={params.block.url}
       className="transition-transform hover:scale-105"
     >
-      <div className="flex h-36 w-36 flex-col items-center rounded-md border border-[#888] bg-[#FFF6EB] p-2">
+      <div className="flex h-36 w-36 flex-col items-center border border-[#FFDAAD] bg-[#FFF6EB] p-2">
         <div>
           <h2 className={`text-center ${params.block.titleClasses ?? ""}`}>
             {params.block.title}
           </h2>
           {params.block.subtitle ? (
-            <h2 className={`m-0 p-0 text-center text-sm text-[#777]`}>
+            <h2 className="m-0 p-0 text-center text-sm text-[#777]">
               {params.block.subtitle}
             </h2>
           ) : (
@@ -62,9 +96,7 @@ export default function Block(params: { block: BlockParams }) {
           )}
         </div>
         {icon !== undefined && (
-          <div
-            className={`flex w-20 items-center justify-center text-[${params.block.iconColor ?? "#444"}]`}
-          >
+          <div className="flex w-20 items-center justify-center text-[#444]">
             {icon}
           </div>
         )}
