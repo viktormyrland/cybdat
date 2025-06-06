31fad7af2b4f5aae4dc1361cdf41497d7159edd3
Viktor Myrland <viktormyrland@gmail.com>
2025-06-06 08:54:21 +0200

    Revert "tfy4125"
    This reverts commit 16fa4fda433171100677cdb96e0527c793238dd0.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
 import Block from "./_components/block";
-import Image from "next/image";
 import { Oxanium, Lato } from "next/font/google";
 
 const oxanium = Oxanium({ weight: "600", subsets: ["latin"] });
@@ -49,39 +47,7 @@ const blocks = [
   },
 ];
 
-function calculateCountdown() {
-  const eksamenDato = new Date("2025-06-03T09:00:00").getTime();
-  const nå = new Date().getTime();
-
-  const diffMs = eksamenDato - nå;
-
-  if (diffMs <= 0) return "Eksamen har startet eller er ferdig!";
-
-  const sek = 1000;
-  const min = sek * 60;
-  const time = min * 60;
-  const dag = time * 24;
-
-  const dager = Math.floor(diffMs / dag);
-  const timer = Math.floor((diffMs % dag) / time);
-  const minutter = Math.floor((diffMs % time) / min);
-  const sekunder = Math.floor((diffMs % min) / sek);
-
-  return `eksamen💀: ${dager}d ${timer}t ${minutter}min ${sekunder}s`;
-}
-
 export default function Home() {
-  const [countdown, setCountdown] = useState("");
-
-  useEffect(() => {
-    setCountdown(calculateCountdown());
-    const interval = setInterval(() => {
-      setCountdown(calculateCountdown());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
-
-
   return (
     <main className="flex h-full min-h-screen w-screen flex-col items-center justify-between bg-gradient-to-b from-[#533A71] to-[#26202E]">
       <div className="flex w-full flex-col items-center gap-2 pt-6">
@@ -91,39 +57,6 @@ export default function Home() {
         {/*  <h3 className={`text-md font-mono font-bold text-[#aaa]`}>
           For de som ikke finner frem
         </h3> */}
-          <div className="w-[80%] bg-white min-h-24 rounded-lg flex flex-col items-center p-4">
-            <div className="flex flex-row gap-2 overflow-x-auto *:object-contain">
-            <Image
-              src="/9v27wo.jpg" 
-              alt="Eksamen bilde"
-              width={200}
-              height={80}
-            />
-            <Image
-              src="/9v291b.jpg" 
-              alt="Eksamen bilde"
-              width={200}
-              height={100}
-            />
-            <Image
-              src="/9v28np.jpg" 
-              alt="Eksamen bilde"
-              width={200}
-              height={100}
-            />
-            <Image
-              src="/9v28q5.jpg" 
-              alt="Eksamen bilde"
-              width={200}
-              height={100}
-            />
-            <Image
-              src="/9v28sc.jpg" 
-              alt="Eksamen bilde"
-              width={200}
-              height={100}
-            /></div>
-          <p className=" p-2 font-bold border-4 border-black">{countdown}</p></div>
         <div
           className={`flex h-full w-[95%] max-w-[1500px] flex-row flex-wrap content-start justify-center gap-3 p-6 ${lato.className}`}
         >
