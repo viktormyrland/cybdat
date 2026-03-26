bb2d4c6168e3820569fca96d9c38c67368f9f761
Viktor Myrland <viktormyrland@gmail.com>
2026-03-26 12:33:14 +0100

    remove countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,19 +47,19 @@ const blocks = [
 const countdowns = [
   {
     name: "Databaser",
-    when: new Date("2026-05-08T09:00:00Z")
+    when: new Date("2026-05-08T09:00:00")
   },
   {
     name: "Statistikk",
-    when: new Date("2026-05-11T09:00:00Z")
+    when: new Date("2026-05-11T09:00:00")
   },
   {
     name: "ADE",
-    when: new Date("2026-05-26T15:00:00Z")
+    when: new Date("2026-05-26T15:00:00")
   },
   {
     name: "Nettverkshåndtering",
-    when: new Date("2026-06-02T15:00:00Z")
+    when: new Date("2026-06-02T15:00:00")
   },
 ]
 
@@ -111,12 +111,18 @@ export default function Home() {
           <a href="https://viktormy.folk.ntnu.no/TDT4145/lecture-plan2.jpg"><p>Databaser - lecture-plan2.jpg</p></a>
           <a href="https://viktormy.folk.ntnu.no/TTT4203/Hjelpehefte.pdf"><p>ADE - Hjelpehefte.pdf</p></a>
         </div>
-        <div className="flex">
+        <div className="flex flex-wrap wrap">
           {countdowns.map((b, _index) => {
+            const formattedDate: string = new Intl.DateTimeFormat('no-NO', {
+              day: 'numeric',
+              month: 'long',
+              hour: '2-digit',
+              minute: '2-digit',
+            }).format(b.when);
             return (
               <div className="bg-white p-2 m-2 rounded-md flex-col justify-center items-center text-center" key={_index}>
                 <h3>{b.name}</h3>
-                <Countdown date={b.when} />
+                <h4>{formattedDate}</h4>
               </div>
             );
           })}
